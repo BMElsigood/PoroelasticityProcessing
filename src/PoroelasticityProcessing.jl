9a76c85f238c8153736b719e6d9193896b8aaaf9
BMElsigood <60468325+BMElsigood@users.noreply.github.com>
2021-04-07 17:45:30 +0100

    export type keymechparams

--- src/PoroelasticityProcessing.jl
+++ src/PoroelasticityProcessing.jl
@@ -3,6 +3,7 @@ module PoroelasticityProcessing
 using DataFrames
 include("types.jl")
 include("functions.jl")
+export keymechparams
 export linBz, linB, linEz, volStrain, linK, linνz, linEν_u, darcyperm, gassmann, gassmannCijkl, axstresssteps,radstresssteps
 
 end
