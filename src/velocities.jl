3521e3c667bb59c6fb87003ea15dada8c4e57e73
BMElsigood <60468325+BMElsigood@users.noreply.github.com>
2021-04-21 13:01:17 +0100

    var name

--- src/velocities.jl
+++ src/velocities.jl
@@ -101,8 +101,8 @@ function surveytocompliancedf(surveyrange,ρ,(θp,vp),(θsh,vsh))
         array[i,4] = θsh
         array[i,5] = vsh
         if n == 1
-            C1,cm,anglep,anglesh = svyCompliancet(survey,ρ,(θp,vp),(θsh,vsh))
-        else C1,cm,anglep,anglesh = svyCompliancet(survey,ρ,(θp,vp),(θsh,vsh),C1)
+            C1,cm,anglep,anglesh = svyCompliancet(i,ρ,(θp,vp),(θsh,vsh))
+        else C1,cm,anglep,anglesh = svyCompliancet(i,ρ,(θp,vp),(θsh,vsh),C1)
         end
         array[i,6] = C1
         ϵ, δ, γ = moduli2thomsen(C1)
