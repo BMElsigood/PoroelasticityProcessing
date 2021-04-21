a7f584eeb8f2ef480f4746c595dacbca4fe88c6f
BMElsigood <60468325+BMElsigood@users.noreply.github.com>
2021-04-21 12:17:21 +0100

    export Bx, Bz for compliances

--- src/PoroelasticityProcessing.jl
+++ src/PoroelasticityProcessing.jl
@@ -5,7 +5,9 @@ using Statistics: mean
 include("types.jl")
 include("functions.jl")
 include("diffusionrampsource.jl")
+include("velocities.jl")
 export keymechparams
 export linBz, linB, linEz, volStrain, linK, linνz, linEν_u, darcyperm, gassmann, gassmannCijkl, axstresssteps,radstresssteps
 export radstressstepsdiff,axstressstepsdiff,pressurerampsolution
+export svyCompliancet, surveytocompliancedf, Bx, Bz, B
 end
